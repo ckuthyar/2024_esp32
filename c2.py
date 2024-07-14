d2058fbdafa2ab806b60b615d920f400cb3e207e
ckuthyar <ckuthyar@gmail.com>
2024-07-14 18:45:14 +0530

    WiFi configuration on ESP-32

---
+++ c2.py
@@ -0,0 +1,16 @@
+import network
+
+ssid = 'BSNL_rtd'
+password = 'uchila2022'
+
+def connect_wifi(ssid, password):
+    station = network.WLAN(network.STA_IF)
+    if not station.isconnected():
+        station.active(True)
+        station.connect(ssid, password)
+        print("......")
+        while not sta_if.isconnected():
+            pass
+    print('Connection successful')
+    print(station.ifconfig())
+connect_wifi(ssid,password)
